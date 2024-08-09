622bc28a07bba73f57156ff89daaefbb2085ceff
Shubham kumar prajapati <66512062+playkashyap@users.noreply.github.com>
2024-08-09 17:07:30 +0530

    updates

--- threee/src/App.jsx
+++ threee/src/App.jsx
@@ -1,27 +1,45 @@
 /* eslint-disable react/no-unknown-property */
-import { Stats, OrbitControls, Circle } from '@react-three/drei'
+import { Stats, OrbitControls, Environment } from '@react-three/drei'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
+import { useControls } from 'leva'
+
+
+function Env() {
+
+
+  const { height, radius, scale } = useControls('Ground', {
+    height: { value: 10, min: 0, max: 100, step: 1 },
+    radius: { value: 115, min: 0, max: 1000, step: 1 },
+    scale: { value: 15, min: 0, max: 1000, step: 1 },
+  })
+
+
+  return (
+    <Environment
+      files="/img/sunset.hdr"
+      background
+      ground={{
+        height: height,
+        radius: radius,
+        scale: scale,
+      }}
+      backgroundBlurriness={0}
+    />
+  )
+}
+
+
 
 export default function App() {
-  const gltf = useLoader(GLTFLoader, '/models/monkey.glb')
-  console.log(gltf)
+  const gltf = useLoader(GLTFLoader, '/models/scene.glb')
+
   return (
-    <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
-      <directionalLight
-        position={[3.3, 1.0, 4.4]}
-        castShadow
-        intensity={Math.PI * 2}
-      />
-      <primitive
-        object={gltf.scene}
-        position={[0, 1, 0]}
-        children-0-castShadow
-      />
-      <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
-        <meshStandardMaterial />
-      </Circle>
-      <OrbitControls target={[0, 1, 0]} />
+    <Canvas camera={{ position: [0, 20, 20], fov : 40 }}>
+      <Env />
+      <directionalLight position={[3.3, 1.0, 4.4]} />
+      <primitive object={gltf.scene} />
+      <OrbitControls target={[0, 1, 0]} autoRotate maxPolarAngle={Math.PI / 2} />
       <axesHelper args={[5]} />
       <Stats />
     </Canvas>
